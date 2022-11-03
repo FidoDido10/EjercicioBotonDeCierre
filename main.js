9e3ca51624723dbaaaaf614ebbf2f072ea4b836b
FidoDido <fernandozaratem@gmail.com>
2022-11-03 16:54:51 +0100

    create

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("caja1").onclick = function () {
+    document.getElementById("caja1").style.display = "none";
+}
+
+document.getElementById("caja2").onclick = () => {
+    document.getElementById("caja2").style.display = "none";
+}
+
+document.getElementById("caja3").onclick = function () {
+    document.getElementById("caja3").style.display = "none";
+}
